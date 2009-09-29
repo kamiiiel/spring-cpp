ea71aa951a39b9014cf58917c39919efd2d9543b
wmlynar <wmlynar@9037b758-a8f9-11de-b6f4-e1be98fab775>
2009-09-29 19:32:18 +0000

    branched - name of the bean factory and output direcory in context file

--- examples/weather-information/generate-bean-factory.bat
+++
@@ -1,2 +0,0 @@
-java -jar ../../bin/spring-cpp.jar --className BeanFactory --outputDir src --contextFile src/app-context.xml
-
--- sources/spring-cpp/src/main/scripts/generate-bean-factory.bat
+++
@@ -1,2 +0,0 @@
-java -jar spring-cpp.jar --className BeanFactory --outputDir your_output_dir --contextFile your_context_file.xml
-
